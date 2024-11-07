e409eccb3f87c9d9e00ee0673b516ab0d5eaf9d8
Nikita Maslov <maslov.n.e@gmail.com>
2024-11-07 14:03:45 +0200

    init hono

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-Deno.serve(() => new Response("Hello, Nikmas Studio!"));
+import { Hono } from 'jsr:@hono/hono';
+
+const app = new Hono();
+
+app.get('/', (c) => c.text("Hello, Nikmas Studio! I'm Hono!"));
+
+Deno.serve(app.fetch);
