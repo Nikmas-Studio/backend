3d331642a00f34770922d3f748c80d482fd72b39
Nikita Maslov <maslov.n.e@gmail.com>
2025-07-12 20:47:21 +0300

    update links based on frontend url

--- utils/get-demo-link-by-book-uri.ts
+++ utils/get-demo-link-by-book-uri.ts
@@ -3,7 +3,7 @@ import { BOOK_MASTER_ENGLISH_WITH_SHERLOCK_HOLMES_URI } from '../constants.ts';
 export function getDemoLinkByBookURI(bookURI: string): string {
   switch (bookURI) {
     case BOOK_MASTER_ENGLISH_WITH_SHERLOCK_HOLMES_URI:
-      return `https://nikmas.studio/${BOOK_MASTER_ENGLISH_WITH_SHERLOCK_HOLMES_URI}/11111/demo`;
+      return `${Deno.env.get('FRONTEND_URL')}/${BOOK_MASTER_ENGLISH_WITH_SHERLOCK_HOLMES_URI}/11111/demo`;
   }
     
   throw new Error(`No demo link defined for book URI: ${bookURI}`);
--- utils/get-promo-link-by-book-uri.ts
+++ utils/get-promo-link-by-book-uri.ts
@@ -3,7 +3,7 @@ import { BOOK_MASTER_ENGLISH_WITH_SHERLOCK_HOLMES_URI } from '../constants.ts';
 export function getPromoLinkByBookURI(bookURI: string): string {
   switch (bookURI) {
     case BOOK_MASTER_ENGLISH_WITH_SHERLOCK_HOLMES_URI:
-      return `https://nikmas.studio/${BOOK_MASTER_ENGLISH_WITH_SHERLOCK_HOLMES_URI}`;
+      return `${Deno.env.get('FRONTEND_URL')}/${BOOK_MASTER_ENGLISH_WITH_SHERLOCK_HOLMES_URI}`;
   }
 
   throw new Error(`No promo link defined for book URI: ${bookURI}`);
