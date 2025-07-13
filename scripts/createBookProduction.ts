312121b7f4d61f618e94b82c750122da03688102
Nikita Maslov <maslov.n.e@gmail.com>
2025-07-13 10:04:01 +0300

    add check for full-access reader to book purchase route

--- scripts/createBookProduction.ts
+++ scripts/createBookProduction.ts
@@ -11,9 +11,10 @@ await asyncLocalStorage.run(new Map(), async () => {
     'https://api.deno.com/databases/4c5ff93a-fe24-4953-8110-72adb0545326/connect',
   );
   const bookRepository = new BookDenoKvRepository(denoKv);
+
   await bookRepository.createBook({
-    title: 'Master Git & GitHub: From Everyday Tasks to Deep Waters',
-    uri: 'book-master-git-and-github',
+    title: 'Master English with Sherlock Holmes',
+    uri: 'book-master-english-with-sherlock-holmes',
     mainPrice: 23,
   });
 });
