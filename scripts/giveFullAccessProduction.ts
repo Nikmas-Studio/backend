a97b2982403c39e2a9f05132e5e98b107cb0d822
Nikita Maslov <maslov.n.e@gmail.com>
2025-07-15 11:02:34 +0300

    update max number of quality checks

--- scripts/giveFullAccessProduction.ts
+++ scripts/giveFullAccessProduction.ts
@@ -12,12 +12,11 @@ await asyncLocalStorage.run(new Map(), async () => {
   const denoKv = await Deno.openKv(
     'https://api.deno.com/databases/4c5ff93a-fe24-4953-8110-72adb0545326/connect',
   );
-  const bookRepository = new ReaderDenoKvRepository(denoKv);
   const readerRepository = new ReaderDenoKvRepository(denoKv);
   await readerRepository.getOrCreateReader({
     email: readerEmail,
     isInvestor: false,
     hasFullAccess: true,
   });
-  await bookRepository.setFullAccessStatus(readerEmail, true);
+  await readerRepository.setFullAccessStatus(readerEmail, true);
 });
--- scripts/giveFullAccessStaging.ts
+++ scripts/giveFullAccessStaging.ts
@@ -10,6 +10,11 @@ await asyncLocalStorage.run(new Map(), async () => {
   const readerEmail = 'maslov.n.e@gmail.com';
 
   const denoKv = await Deno.openKv("https://api.deno.com/databases/de14b4e4-b269-4b43-bd8e-d91ca4dddf84/connect");
-  const bookRepository = new ReaderDenoKvRepository(denoKv);
-  await bookRepository.setFullAccessStatus(readerEmail, true);
+  const readerRepository = new ReaderDenoKvRepository(denoKv);
+  await readerRepository.getOrCreateReader({
+    email: readerEmail,
+    isInvestor: false,
+    hasFullAccess: true,
+  });
+  await readerRepository.setFullAccessStatus(readerEmail, true);
 });
